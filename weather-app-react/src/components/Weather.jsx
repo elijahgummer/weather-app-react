f902e532e54b0c0f201da68a3fe9ad4e983f5f21
Elijah Gummer <elijahgummer5@gmail.com>
2024-06-08 10:35:49 +1000

    changed the code added an image

--- weather-app-react/src/components/Weather.jsx
+++ weather-app-react/src/components/Weather.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Weather.css'
+import search_icon from '../Assets/search.png '
 
 const Weather = () => {
   return (
     <div className='weather'>
       <div className="search-bar">
         <input type="text" placeholder='Search'/>
+        <img src="{search_icon}" alt="" />
       </div>
     </div>
   )
