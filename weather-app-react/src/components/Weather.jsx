457c9bb0aea7865345e08b17be7ecc3cafdb208b
Elijah Gummer <elijahgummer5@gmail.com>
2024-06-08 11:07:20 +1000

    changed the speed

--- weather-app-react/src/components/Weather.jsx
+++ weather-app-react/src/components/Weather.jsx
@@ -20,6 +20,22 @@ const Weather = () => {
       <img src={clear_icon} alt="" className='weather-icon'/>
       <p className='temperature'>16°C</p>
       <p className='location'>London</p>
+      <div className="weather-data">
+        <div className="col">
+          <img src={humidity_icon} alt="" />
+          <div>
+            <p>91%</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src={wind_icon} alt="" />
+          <div>
+            <p>3.6</p>
+            <span>Wind Speed</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
