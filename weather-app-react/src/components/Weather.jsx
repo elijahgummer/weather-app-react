9aaa1daadc79d0938fe539539b1195a4afebe39f
Elijah Gummer <elijahgummer5@gmail.com>
2024-06-08 10:57:44 +1000

    changed the code for the importing the images

--- weather-app-react/src/components/Weather.jsx
+++ weather-app-react/src/components/Weather.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import './Weather.css'
 import search_icon from '../Assets/search.png'
 import clear_icon from '../Assets/clear.png'
-import search_icon from '../Assets/search.png'
-import search_icon from '../Assets/search.png'
-import search_icon from '../Assets/search.png'
+import cloud_icon from '../Assets/cloud.png'
+import drizzle_icon from '../Assets/drizzle.png'
+import humidity_icon from '../Assets/humidity.png'
+import rain_icon from '../Assets/rain.png'
+import snow_icon from '../Assets/snow.png'
+import wind_icon from '../Assets/wind.png'
 
 
 const Weather = () => {
