52f768c1ae9dc5ecc6bcfcb979ea8dd15d4f6572
Elijah Gummer <elijahgummer5@gmail.com>
2024-06-09 15:40:03 +1000

    addeding the search feature

--- weather-app-react/src/components/Weather.jsx
+++ weather-app-react/src/components/Weather.jsx
@@ -11,6 +11,18 @@ import wind_icon from '../Assets/wind.png'
 
 
 const Weather = () => {
+    const search = async (city)=> {
+      try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+        
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+
+      } catch (error) {
+
+      }
+    }
   return (
     <div className='weather'>
       <div className="search-bar">
